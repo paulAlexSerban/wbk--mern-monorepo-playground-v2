a6de6ddd132215d21c29535f963f90dab746f4fb
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-12 23:58:27 +0200

    hbs_03 | converted all html to hbs

--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,22 @@
-
 const path = require('path');
+
 const express = require('express');
-const router = express.Router();
 
 const rootDir = require('../util/path');
 const adminData = require('./admin');
 
-/**
- * 1. the use of `path.join()` makes it possible to use Windows and Linux as OS without many issues
- *    - Windows uses paths with back-slash '..\views\shop.html'
- *    - Linux uses paths with forward-slash '..\views\shop.html'/views/shop.html
- */
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir,'views', 'shop.html')); /* 1 */
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
